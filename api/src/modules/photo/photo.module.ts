4cb01e790b4b3fd79048db91da194a4988b474db
melnik <melnik@bifit.com>
2021-09-27 16:47:24 +0300

    Api service. Photo. Generate new module

--- api/src/modules/photo/photo.module.ts
+++ api/src/modules/photo/photo.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PhotoController } from './photo.controller';
+import { PhotoService } from './photo.service';
 
-@Module({})
+@Module({
+  controllers: [PhotoController],
+  providers: [PhotoService],
+})
 export class PhotoModule {}
