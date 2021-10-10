1a034a3372ff4e249f51c1c2d058ebb0bc48f4d7
melnik <Boherave21>
2021-10-10 18:31:33 +0300

    Api service. Set application port

--- api/src/configs/env-validator.config.ts
+++ api/src/configs/env-validator.config.ts
@@ -4,6 +4,7 @@ import * as Joi from 'joi';
 export const envValidatorSchema = (): ConfigModuleOptions => {
   return {
     validationSchema: Joi.object({
+      APP_PORT: Joi.number().positive().default(3000),
       MONGODB_HOST: Joi.string().required().default('api-db'),
       MONGODB_PORT: Joi.number().positive().default(27017),
       MONGODB_DATABASE: Joi.string().required().default('vdomkota'),
--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const PORT = process.env.APP_PORT || 3000;
+  await app.listen(PORT);
+  console.log(`API сервис запущен на ${PORT} порту`)
 }
 bootstrap();
